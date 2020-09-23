dcf48433297e262bbe91b3bf9b544a9d3edbb7a0
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-09-23 11:12:18 +0100

    Redoing Task:
    - Changed canvas size.
    - Added purple rect. with its own "shadow".

--- Main Exercises/Introduction/sketch.js
+++ Main Exercises/Introduction/sketch.js
@@ -1,10 +1,16 @@
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(windowWidth/2, windowHeight/2);
     background(0);
 };
 
 function draw() {
-    // Basic Rect:
-    fill(255);
-    rect(0, 0, 300, 300);
+    // Shadow for Purple Rect.
+    noStroke();
+    fill(25);
+    rect(25, 25, 200, 200);
+
+    // Purple Rect.
+    noStroke();
+    fill(102, 51, 153);
+    rect(25/2, 25/2, 200, 200);
 };
