3b4bc9126c9e6aadcb738e04a6b0b9c1710aa1de
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-09-23 11:13:47 +0100

    Redoing Task Commit #3:
    - Created "Illusional 3D" rectangle from using basic loop function.

--- Main Exercises/Introduction/sketch.js
+++ Main Exercises/Introduction/sketch.js
@@ -1,16 +1,23 @@
+rectLoop = 75;
+
 function setup() {
     createCanvas(windowWidth/2, windowHeight/2);
     background(0);
-};
+}
 
 function draw() {
     // Shadow for Purple Rect.
-    noStroke();
-    fill(25);
-    rect(25, 25, 200, 200);
+    // noStroke();
+    // fill(25);
+    // rect(25, 25, 200, 200);
 
     // Purple Rect.
-    noStroke();
-    fill(102, 51, 153);
-    rect(25/2, 25/2, 200, 200);
-};
+    // noStroke();
+    // fill(102, 51, 153);
+    // rect(25/2, 25/2, 200, 200);
+
+    // Looped Rect (3D Illusional Rectangle):
+    for (let i = 0; i < rectLoop; i++) {
+        rect(i, i, 300, 300);
+    };
+}
