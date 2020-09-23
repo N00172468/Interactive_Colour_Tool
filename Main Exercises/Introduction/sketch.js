2253158c0b84dc904ac9b6f150a49c6cb174efa0
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-09-23 11:22:10 +0100

    Created HSB Canvas:
    Inputted and understood loop to create HSB Canvas.

--- Main Exercises/Introduction/sketch.js
+++ Main Exercises/Introduction/sketch.js
@@ -1,8 +1,11 @@
 rectLoop = 75;
 
 function setup() {
-    createCanvas(windowWidth/2, windowHeight/2);
+    createCanvas(windowWidth, windowHeight);
     background(0);
+
+    // RGB to HSB:
+    colorMode(HSB, width, height, 100); 
 }
 
 function draw() {
@@ -17,7 +20,22 @@ function draw() {
     // rect(25/2, 25/2, 200, 200);
 
     // Looped Rect (3D Illusional Rectangle):
-    for (let i = 0; i < rectLoop; i++) {
-        rect(i, i, 300, 300);
-    };
+    // for (let i = 0; i < rectLoop; i++) {
+    //     rect(i, i, 300, 300);
+    // };
+
+    //-----------------------------------------------//
+
+    // HSB Canvas:
+    let stepX = 15;
+    let stepY = 15;
+
+    for (let y = 0; y < height; y = y + stepY) {
+        for (let x = 0; x < width; x = x + stepX) {
+            noStroke();
+            fill(x, height - y, 100);
+            rect(x, y, stepX, stepY);
+        }
+    }
+
 }
