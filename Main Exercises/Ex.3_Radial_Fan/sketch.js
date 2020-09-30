164854671a68fe15461628520aef146d5547d8d5
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-09-30 09:55:57 +0100

    Added Radial Fan:
    - Created HSB Radial Fan.
    - Incomplete to make it my own (i.e. customisation).

--- Main Exercises/Ex.3_Radial_Fan/sketch.js
+++ Main Exercises/Ex.3_Radial_Fan/sketch.js
@@ -1,17 +1,24 @@
-let numOfSegments = 12;
+// let numOfSegments = 12;
+let numOfSegments = 360;
 let radius = 100;
 
 function setup() {
     createCanvas(500, 500);
+    background(0);
 
     // RGB to HSB:
-    colorMode(HSB, width, height, 100); 
+    colorMode(HSB, 360, 100, 100); 
 
     // Angle Mode:
     angleMode(DEGREES);
+
+    // No Stroke:
+    noStroke();
 }
 
 function draw() {
+    // background(125, 125, 200);
+    fill(200, 0, 0);
     let stepAngle = 360/numOfSegments;
 
     beginShape(TRIANGLE_FAN);
@@ -19,6 +26,7 @@ function draw() {
         for (let a = 0; a <= 360; a += stepAngle) {
             let vx = radius * cos(a) + 250;
             let vy = radius * sin(a) + 250;
+            fill(a, 100, 100);
             vertex(vx, vy);
         }
     endShape(CLOSE);
