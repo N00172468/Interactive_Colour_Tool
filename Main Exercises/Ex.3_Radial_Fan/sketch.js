0120bb69fcb572453bde3be4ece95aec18d4e7f8
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-09-30 10:08:19 +0100

    Update on Ex.3 - Radial Fan:
    - Cleaned up codes.
    - Added additional comments.

--- Main Exercises/Ex.3_Radial_Fan/sketch.js
+++ Main Exercises/Ex.3_Radial_Fan/sketch.js
@@ -6,27 +6,22 @@ function setup() {
     createCanvas(500, 500);
     background(0);
 
-    // RGB to HSB:
-    colorMode(HSB, 360, 100, 100); 
-
-    // Angle Mode:
+    colorMode(HSB, 360, 100, 100);
     angleMode(DEGREES);
-
-    // No Stroke:
     noStroke();
 }
 
 function draw() {
-    // background(125, 125, 200);
     fill(200, 0, 0);
     let stepAngle = 360/numOfSegments;
 
+    // Begin Triangle Fan (based on P5.js docs.):
     beginShape(TRIANGLE_FAN);
-        vertex(250, 250);
+        vertex(250, 250); // Centre of diameter
         for (let a = 0; a <= 360; a += stepAngle) {
             let vx = radius * cos(a) + 250;
             let vy = radius * sin(a) + 250;
-            fill(a, 100, 100);
+            fill(a, 100, 100); // Created Radial Colour Hue (HSB variant)
             vertex(vx, vy);
         }
     endShape(CLOSE);
