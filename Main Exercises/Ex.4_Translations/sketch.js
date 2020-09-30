7e987c8f5cf1d40aea03c18641720ac7fad626c5
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-09-30 10:15:10 +0100

    Added Ex.4 - Translation File

---
+++ Main Exercises/Ex.4_Translations/sketch.js
@@ -0,0 +1,16 @@
+// let numOfSegments = 12;
+let numOfSegments = 360;
+let radius = 100;
+
+function setup() {
+    createCanvas(500, 500);
+    background(0);
+
+    colorMode(HSB, 360, 100, 100);
+    angleMode(DEGREES);
+    noStroke();
+}
+
+function draw() {
+
+}
